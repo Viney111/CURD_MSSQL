a982794bbfb3dec941847f7f81a5aec5c3dc0f21
Viney <vineykhaneja999@gmail.com>
2022-05-02 20:11:35 +0530

    [Viney] Added: Setting up a connection MSSQL

--- connection_sql.py
+++ connection_sql.py
@@ -1,1 +1,32 @@
+'''
+@Author: Viney Khaneja
+@Date: 2021-05-02 13:56
+@Last Modified by: Viney Khaneja
+@Last Modified time: None
+@Title : CURD-Operations
+'''
+# Importing Module for setting connection with MSSQL
+import pyodbc
+
 print("Welcome to MSSQL Connection in Python")
+
+server = "LAPTOP-IUMGL5A5"
+database = "customer_services"
+username = "LAPTOP-IUMGL5A5\Kashish Manchanda"
+
+# Connection String for connecting MSSQL
+connection_str = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+'; Trusted_Connection=yes;')
+mycursor = connection_str.cursor()
+
+# Reading Data from File
+try:
+    mycursor.execute("SELECT * FROM customer_info")
+    for i in mycursor:
+        print(i)
+except Exception as ex:
+    print(ex)
+finally:
+    # Closing the cursor and connection finally
+    mycursor.close()
+    connection_str.close()
